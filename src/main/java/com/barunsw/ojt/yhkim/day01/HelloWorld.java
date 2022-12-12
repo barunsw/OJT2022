6f30de08369b74fd98144dd579d88796191f6bf1
배수현 <sbae75@gmail.com>
2022-12-12 10:24:59 +0900

    [배수현] 개인 패키지 생성

---
+++ src/main/java/com/barunsw/ojt/yhkim/day01/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.barunsw.ojt.yhkim.day01;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+	private static Logger LOGGER = LoggerFactory.getLogger(HelloWorld.class);
+	
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		LOGGER.debug("Hello World");
+	}
+}
