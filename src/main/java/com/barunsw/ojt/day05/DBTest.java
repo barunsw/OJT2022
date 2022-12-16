df4f96330d1e5e3b3ab20f557588e8077fe5e89c
LeeYoonJung <imj1017@naver.com>
2022-12-16 13:25:23 +0900

    [이윤정] OJT 5일차

---
+++ src/main/java/com/barunsw/ojt/day05/DBTest.java
@@ -0,0 +1,26 @@
+package com.barunsw.ojt.day05;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.barunsw.ojt.intf.AddressBookInterface;
+import com.barunsw.ojt.vo.AddressBookVo;
+
+public class DBTest {
+	private static Logger LOGGER = LoggerFactory.getLogger(DBTest.class);
+
+	public static void main(String[] args) throws Exception {
+		AddressBookInterface addressBook = new FileAddressBookImpl();
+		
+		addressBook.selectAddressBook(null);  // 연락처 조회문
+		
+		AddressBookVo paramVo = new AddressBookVo();
+		paramVo.setName("홍길동");
+		paramVo.setBirthday("1994");
+		paramVo.setPhoneNumber("1234");
+		paramVo.setAddress("서울");
+		
+		//addressBook.insertAddressBook(paramVo);
+		
+	}
+}
