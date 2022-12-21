baa7ce8d6ec1c9e6b06deaef52a3d9a7dc05e65f
yhkim <hie0908@naver.com>
2022-12-21 09:23:13 +0900

    [김연희] MybatisAddressBookImpl 구현

--- src/main/java/com/barunsw/ojt/yhkim/day07/DBTest.java
+++ src/main/java/com/barunsw/ojt/yhkim/day07/DBTest.java
@@ -1,89 +1,64 @@
 package com.barunsw.ojt.yhkim.day07;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.barunsw.ojt.constants.Gender;
-import com.barunsw.ojt.yhkim.day07.AddressBookDao;
-import com.barunsw.ojt.yhkim.day07.AddressBook;
+import com.barunsw.ojt.intf.AddressBookInterface;
+import com.barunsw.ojt.vo.AddressBookVo;
 
 public class DBTest {
 	private static Logger LOGGER = LoggerFactory.getLogger(DBTest.class);
 
 	public static void main(String[] args) throws Exception {
-		
-		SqlSessionFactory sqlSessionFactory = SqlSessionFactoryManager.getSqlSessionFactory();
-		
-		try (SqlSession session = sqlSessionFactory.openSession()){
-			AddressBookDao mapper = session.getMapper(AddressBookDao.class);
-			
-			List<AddressBook> addressBookList = mapper.selectAddressBook(null);
-			for (AddressBook b : addressBookList) {
-				LOGGER.debug(b.toString());
-			}
-			
-			AddressBook insertData = new AddressBook();
-			insertData.setName("김하나");
-			insertData.setBirthday("2000");
-			insertData.setGender(Gender.MAN);
-			insertData.setPhoneNumber("010-1344-1234");
-			insertData.setAddress("강릉");
-			
-			int insertResult = mapper.insertAddressBook(insertData);
-			
-			addressBookList = mapper.selectAddressBook(null);
-			for (AddressBook b : addressBookList) {
-				LOGGER.debug(b.toString());
-			}
-			
-			AddressBook updateData = new AddressBook();
-			updateData.setSeq(126);
-			updateData.setName("abc");
-			updateData.setBirthday("2000");
-			updateData.setGender(Gender.WOMAN);
-			updateData.setPhoneNumber("010-1344-1234");
-			updateData.setAddress("강릉");
-			
-			int updateResult = mapper.updateAddressBook(updateData);
-			
-			addressBookList = mapper.selectAddressBook(null);
-			for (AddressBook b : addressBookList) {
-				LOGGER.debug(b.toString());
-			}
-			
-			AddressBook deleteData = new AddressBook();
-			deleteData.setSeq(135);
-			
-			int deleteResult = mapper.deleteAddressBook(deleteData);
-			
-			addressBookList = mapper.selectAddressBook(null);
-			for (AddressBook b : addressBookList) {
-				LOGGER.debug(b.toString());
-			}
 
-			AddressBook findData = new AddressBook();
-			findData.setName("나");
-			
-			addressBookList = mapper.selectAddressByName(findData);
-			for (AddressBook b : addressBookList) {
-				LOGGER.debug(b.toString());
-			}
-			
-			AddressBook findData2 = new AddressBook();
-			findData2.setSeq(137);
-			
-			LOGGER.debug(mapper.selectAddressBySeq(findData2).toString());
-			
-			session.commit();
-			
+		AddressBookInterface bookService = new MybatisAddressBookImpl();
+
+		List<AddressBookVo> addressBookList = bookService.selectAddressBook(null);
+		for(AddressBookVo b : addressBookList) {
+			LOGGER.debug(b.toString());
 		}
-		catch(Exception e) {
-			LOGGER.error(e.getMessage());
+
+		AddressBookVo insertData = new AddressBookVo();
+		insertData.setName("김하나");
+		insertData.setBirthday("2000");
+		insertData.setGender(Gender.MAN);
+		insertData.setPhoneNumber("010-1344-1234");
+		insertData.setAddress("강릉");
+
+		int insertResult = bookService.insertAddressBook(insertData);
+
+		addressBookList = bookService.selectAddressBook(null);
+		for (AddressBookVo b : addressBookList) {
+			LOGGER.debug(b.toString());
 		}
+
+		AddressBookVo updateData = new AddressBookVo();
+		updateData.setSeqNum(126);
+		updateData.setName("abc");
+		updateData.setBirthday("2000");
+		updateData.setGender(Gender.WOMAN);
+		updateData.setPhoneNumber("010-1344-1234");
+		updateData.setAddress("강릉");
+
+		int updateResult = bookService.updateAddressBook(updateData);
+
+		addressBookList = bookService.selectAddressBook(null);
+		for (AddressBookVo b : addressBookList) {
+			LOGGER.debug(b.toString());
+		}
+
+		AddressBookVo deleteData = new AddressBookVo();
+		deleteData.setSeqNum(135);
+
+		int deleteResult = bookService.deleteAddressBook(deleteData);
+
+		addressBookList = bookService.selectAddressBook(null);
+		for (AddressBookVo b : addressBookList) {
+			LOGGER.debug(b.toString());
+		}
+
 	}
 }
