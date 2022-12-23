f2b8fc4ba422894ab2c2956146c876fa6a1d32ff
yhkim <hie0908@naver.com>
2022-12-23 17:59:47 +0900

    [김연희] ClockPanel 수정

--- src/main/java/com/barunsw/ojt/yhkim/day11/ClockPanel.java
+++ src/main/java/com/barunsw/ojt/yhkim/day11/ClockPanel.java
@@ -13,13 +13,11 @@ import javax.swing.JPanel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+
 public class ClockPanel extends JPanel {
 	private static Logger LOGGER = LogManager.getLogger(ClockPanel.class);
 	
 	private Timer timer;
-	private int sec, min, hour, xSec, ySec, xMin, yMin, xHour, yHour;
-	private int ox = 300, oy = 300;
-	private int r = 200;
 	
 	public ClockPanel() {
 		try {
@@ -52,74 +50,33 @@ public class ClockPanel extends JPanel {
 	}
 	
 	private void initData() {
-		
-		Calendar cal = Calendar.getInstance();
-		sec = cal.get(Calendar.SECOND);
-		min = cal.get(Calendar.MINUTE);
-		hour = cal.get(Calendar.HOUR);
-
-		LOGGER.debug(hour + "시 "+ min + "분 " + sec + "초");
-		
-		sec = sec * 6 - 90;
-		xSec = (int) (150 * Math.cos(Math.PI * sec / 180) + ox);
-		ySec = (int) (150 * Math.sin(Math.PI * sec / 180) + oy);
-
-		min = min * 6 + sec / 60 - 90;
-		xMin = (int) (125 * Math.cos(Math.PI * min / 180) + ox);
-		yMin = (int) (125 * Math.sin(Math.PI * min / 180) + oy);
-		
-		hour = hour * 30 + hour / 2 - 90;
-		xHour = (int) (100 * Math.cos(Math.PI * hour / 180) + ox);
-		yHour = (int) (100 * Math.sin(Math.PI * hour / 180) + oy);
-
+		// 시침의 위치
+		// 분침의 위치
+		// 초침의 위치
 	}
 	
 	// ClockPanel 객체의  repaint 메소드 호출하면 paintComponent가 호출
 	
 	@Override
 	public void paintComponent(Graphics g) {
+		//Graphics2D g2d = (Graphics2D)g;
+		LOGGER.debug("paintComponent");
 		
 		g.setColor(Color.white);
 		g.fillRect(0,  0, this.getWidth(), this.getHeight());
 
 		g.setColor(Color.black);
 		g.setFont(new Font(Font.DIALOG_INPUT, Font.BOLD, 20));
-		g.drawString("아날로그 시계", 50, 50);
-
-		g.setColor(Color.black);
-		g.drawOval(100, 100, 400, 400);
-
-		g.setColor(Color.black);
-
-		g.setFont(new Font(Font.DIALOG_INPUT, Font.PLAIN, 20));
-		for (int i = 1; i < 13; i++) {
-			int x = (int) ((r - 30) * Math.cos(Math.PI * ((30 * i) - 90) / 180)+ ox);
-			int y = (int) ((r - 30) * Math.sin(Math.PI * ((30 * i) - 90) / 180)+ oy);
-			
-			g.drawString(Integer.toString(i), x, y);
-		}
-		
-		for (int i = 0; i <= 360; i += 6) { // 시계 테두리 눈금
-			int x1 = (int) (r * Math.sin(Math.PI * i / 180) + ox);
-			int y1 = (int) (r * Math.cos(Math.PI* i / 180) + oy);
-			int x2 = (int) ((r - 5) * Math.sin(Math.PI * i / 180) + ox);
-			int y2 = (int) ((r - 5) * Math.cos(Math.PI * i / 180) + oy);
-
-			if (i % 30 == 0) { // 큰 눈금
- 				x2 = (int) ((r - 20) * Math.sin(Math.PI * i / 180) + ox);
-				y2 = (int) ((r - 20) * Math.cos(Math.PI * i / 180) + oy);
-			}
-			g.drawLine(x1, y1, x2, y2);
-		}
-		
-		g.setColor(Color.red);
-		g.drawLine(ox, oy, xSec, ySec);
+		g.drawString("테스트", 100, 100);
 
 		g.setColor(Color.blue);
-		g.drawLine(ox, oy, xMin, yMin);
-		
-		g.setColor(Color.green);
-		g.drawLine(ox, oy, xHour, yHour);
+		g.drawOval(200, 200, 200, 100);
 
+		g.setColor(Color.green);
+		g.drawLine(300, 300, 400, 400);
+		
+		// 중앙의 점과 시침의 위치를 이어서 선을 긋는다.
+		// 중앙의 점과 분침의 위치를 이어서 선을 긋는다.
+		// 중앙의 점과 초침의 위치를 이어서 선을 긋는다.
 	}
 }
