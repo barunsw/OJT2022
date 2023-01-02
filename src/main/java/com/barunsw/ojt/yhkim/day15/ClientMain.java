0beaf967f9e9267456e05e35b726ff63bffba5f4
yhkim <branlab.kyh@gmail.com>
2023-01-02 17:33:43 +0900

    [김연희] 16일차 소켓

---
+++ src/main/java/com/barunsw/ojt/yhkim/day15/ClientMain.java
@@ -0,0 +1,34 @@
+package com.barunsw.ojt.yhkim.day15;
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+
+import javax.swing.UIManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClientMain {
+	private static Logger LOGGER = LoggerFactory.getLogger(ClientMain.class);
+
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		}
+		catch (Exception ex) {
+			LOGGER.error(ex.getMessage() + ex);
+		}
+		
+		ClientFrame frame = new ClientFrame();
+		
+		Dimension scrDim = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		int xPos = (scrDim.width - ClientFrame.WIDTH) / 2;
+		int yPos = (scrDim.height - ClientFrame.HEIGHT) / 2;
+
+		frame.setBounds(new Rectangle(xPos, yPos, ClientFrame.WIDTH, ClientFrame.HEIGHT));
+		frame.setVisible(true);	
+	}
+
+}
